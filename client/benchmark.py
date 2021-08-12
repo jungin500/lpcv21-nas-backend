76e712e70af43595118be8cb97d9a21805026ef4
LimeOrangePie <ji5489@gmail.com>
2021-08-12 14:46:02 +0900

    Changed to use torch.no_grad()

--- client/benchmark.py
+++ client/benchmark.py
@@ -23,39 +23,40 @@ if __name__ == '__main__':
         sys.exit(1)
 
     start_time = time.time()
-    t_model = torch.jit.load(sys.argv[1], map_location=torch.device('cpu'))
+    t_model = torch.jit.load(sys.argv[1], map_location=torch.device('cpu')).eval()
     end_time = time.time()
     print('Loading model took %.0fms' % ((end_time - start_time) * 1000.0, ))
 
     input_shape = [1, 3, *[int(i) for i in sys.argv[2:]]]
     
-    print("Warming up ...", end=' ', flush=True)
-    for _ in range(3):
-        input_feature = torch.zeros(input_shape)
-        output = t_model(input_feature)
-        if type(output) == torch.Tensor or type(output) == np.ndarray:
-            output_shape = list(output.shape)
-        elif type(output) == list or type(output) == tuple:
-            output_shape = len(output)
-        else:
-            output_shape = str(type(input)) + ": " + str(output)
-        del input_feature, output
-    print("Done")
-
-    items = 20
-    rq.start(model_name=sys.argv[1])
-
-    start_time = time.time()
-    print("Inference remaining ...", end=' ', flush=True)
-    for i in range(items, 0, -1):
-        print(str(i), end=' ', flush=True)
-        input_feature = torch.zeros(input_shape)
-        output = t_model(input_feature)
-        del input_feature, output
-    print()
-    end_time = time.time()
-    print('Inspecting model with %d items took %.0fms' % (items, (end_time - start_time) * 1000.0))
-    total_energy_consumption_mwh = rq.end(model_name=sys.argv[1], elapsed_time_sec=int(end_time - start_time), total_frames=items)
+    with torch.no_grad():
+        print("Warming up ...", end=' ', flush=True)
+        for _ in range(3):
+            input_feature = torch.zeros(input_shape)
+            output = t_model(input_feature)
+            if type(output) == torch.Tensor or type(output) == np.ndarray:
+                output_shape = list(output.shape)
+            elif type(output) == list or type(output) == tuple:
+                output_shape = len(output)
+            else:
+                output_shape = str(type(input)) + ": " + str(output)
+            del input_feature, output
+        print("Done")
+    
+        items = 20
+        rq.start(model_name=sys.argv[1])
+    
+        start_time = time.time()
+        print("Inference remaining ...", end=' ', flush=True)
+        for i in range(items, 0, -1):
+            print(str(i), end=' ', flush=True)
+            input_feature = torch.zeros(input_shape)
+            output = t_model(input_feature)
+            del input_feature, output
+        print()
+        end_time = time.time()
+        print('Inspecting model with %d items took %.0fms' % (items, (end_time - start_time) * 1000.0))
+        total_energy_consumption_mwh = rq.end(model_name=sys.argv[1], elapsed_time_sec=int(end_time - start_time), total_frames=items)
 
     metrics = get_model_metrics(t_model, input_shape)
     print("Metric:", metrics)
